2616d9ca5c6a2c6ce84c7e60b978485472105e96
Feller <iamliurenjie@gmail.com>
2021-12-31 16:30:36 +0800

    Update pi.py

--- pi.py
+++ pi.py
@@ -1,77 +1,130 @@
-import re
-
-import requests
+# 获取哔哩哔哩直播的真实流媒体地址，默认获取直播间提供的最高画质
+# qn=150高清
+# qn=250超清
+# qn=400蓝光
+# qn=10000原画
 import json
 import time
-from datetime import datetime
-from bs4 import BeautifulSoup
-from lxml import etree
-from django.template.defaultfilters import striptags
+
+import requests
+import pi
+
+
+class BiliBili:
+
+    def __init__(self, rid):
+        """
+        有些地址无法在PotPlayer播放，建议换个播放器试试
+        Args:
+            rid:
+        """
+        rid = rid
+        self.header = {
+            'User-Agent': 'Mozilla/5.0 (iPod; CPU iPhone OS 14_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, '
+                          'like Gecko) CriOS/87.0.4280.163 Mobile/15E148 Safari/604.1',
+        }
+        # 先获取直播状态和真实房间号
+        r_url = 'https://api.live.bilibili.com/room/v1/Room/room_init'
+        param = {
+            'id': rid
+        }
+        with requests.Session() as self.s:
+            res = self.s.get(r_url, headers=self.header, params=param).json()
+        if res['msg'] == '直播间不存在':
+            raise Exception(f'bilibili {rid} {res["msg"]}')
+        live_status = res['data']['live_status']
+        if live_status != 1:
+            raise Exception(f'bilibili {rid} 未开播')
+        self.real_room_id = res['data']['room_id']
+
+    def get_real_url(self, current_qn: int = 10000) -> dict:
+        url = 'https://api.live.bilibili.com/xlive/web-room/v2/index/getRoomPlayInfo'
+        param = {
+            'room_id': self.real_room_id,
+            'protocol': '0,1',
+            'format': '0,1,2',
+            'codec': '0,1',
+            'qn': current_qn,
+            'platform': 'h5',
+            'ptype': 8,
+        }
+        res = self.s.get(url, headers=self.header, params=param).json()
+        stream_info = res['data']['playurl_info']['playurl']['stream']
+        qn_max = 0
+
+        for data in stream_info:
+            accept_qn = data['format'][0]['codec'][0]['accept_qn']
+            for qn in accept_qn:
+                qn_max = qn if qn > qn_max else qn_max
+        if qn_max != current_qn:
+            param['qn'] = qn_max
+            res = self.s.get(url, headers=self.header, params=param).json()
+            stream_info = res['data']['playurl_info']['playurl']['stream']
+
+        stream_urls = {}
+        # flv流无法播放，暂修改成获取hls格式的流，
+        for data in stream_info:
+            format_name = data['format'][0]['format_name']
+            if format_name == 'ts':
+                base_url = data['format'][-1]['codec'][0]['base_url']
+                url_info = data['format'][-1]['codec'][0]['url_info']
+                for i, info in enumerate(url_info):
+                    host = info['host']
+                    extra = info['extra']
+                    stream_urls[f'[线路{i + 1}]:'] = f'{host}{base_url}{extra}'
+                break
+        return stream_urls
+
+
+def get_real_url(rid):
+    try:
+        bilibili = BiliBili(rid)
+        return bilibili.get_real_url()
+
+
+    except Exception as e:
+        print('Exception：', e)
+        return False
 
 
 
-headers={'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'}
+def binali():
+    rel = get_real_url(21314309)
+    s=''
+    p = ''
 
-def roominfostr(ridd):
-    ridd=str(ridd)
-
-    response = requests.get("https://api.live.bilibili.com/xlive/web-room/v1/index/getInfoByRoom?room_id=" + ridd,
-                            headers=headers, timeout=10)
-    live_status = json.loads(response.text)['data']['room_info']['live_status']  # 开播状态 1为已开播
-    room_id = json.loads(response.text)['data']['room_info']['room_id']
-    uid = json.loads(response.text)['data']['room_info']['uid']
-    title = json.loads(response.text)['data']['room_info']['title']
-    description = json.loads(response.text)['data']['room_info']['description']
-    live_start_time = json.loads(response.text)['data']['room_info']['live_start_time']
-    online = json.loads(response.text)['data']['room_info']['online']
-
-    # 使用time
-    timeStamp = live_start_time
-    timeArray = time.localtime(timeStamp)
-
-    otherStyleTime = time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
-    otherStyleTime = str(otherStyleTime)
-    online = str(online)
-    online = online + '人'
-    room_id = str(room_id)
-    uid = str(uid)
-    description = str(description)
-    description = striptags(description)
-
-#     response = etree.HTML(text=description)
-#     # print(dir(response))
-#     # print(response.xpath('string(.)'))
-#     line = response.xpath('string(.)')
-#     # line = '<p>宁波大学</p>'
-
-#     regex = r'</?p>'
-
-#     result = re.sub(regex, "", line, re.I)
-
-#     # print(result)
-
-#     description = result
-
-    if live_status == 1:
-        live_status = '已开播'
-    else:
-        live_status = '未开播'
-        otherStyleTime = ''
-
-    roominfostr =  '本次直播主题：' + title + '\n' + '直播间简介：' + description   + '\n'+ '本次开播时间：' + otherStyleTime + '\n' + '当前直播人气：' + online + '\n' + '房间号：' + room_id + '\n' + '用户UID：' + uid + '\n' + '开播状态：' + live_status + '\n'
-
-    print(roominfostr)
-    return roominfostr
+    for key in rel:
 
 
-def roominfotitle(ridd):
-    ridd = str(ridd)
+        xl=key  # 拿到key
+        dz=rel[key]  # 拿到value，实现对value的遍历
+        s='\n'+xl+'\n'+dz+'\n'
+        p += s
 
-    response = requests.get("https://api.live.bilibili.com/xlive/web-room/v1/index/getInfoByRoom?room_id=" + ridd,
-                            headers=headers, timeout=10)
 
-    title = json.loads(response.text)['data']['room_info']['title']
-    title=str(title)
-    title=striptags(title)
-    return title
+    return p
 
+
+if __name__ == '__main__':
+    try:
+        fasongneir3 = str(binali())
+        titl = pi.roominfotitle(21314309)
+        titl = str(titl)
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+            'accept-encoding': "gzip, deflate, br"
+        }
+
+        url = 'https://tdtt.top/send'
+
+        fasongneir = pi.roominfostr(21314309) + '真实地址:\n' + fasongneir3
+        fasongneir = str(fasongneir)
+
+        data = {'content': fasongneir, 'title': titl, 'alias': 'liurenjie520'}
+        r = requests.post(url, data, headers=headers)
+
+        print(r.text)
+    except:
+        print("未开播")
+    
+    
